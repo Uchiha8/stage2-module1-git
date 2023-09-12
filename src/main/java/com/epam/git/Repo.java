86ae095133f7ff2669e152082e762568dd5ce63f
2110107 <najimovm11@gmail.com>
2023-09-12 14:34:42 +0500

    done

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/vdmpeniel/stage2-module2-vcs-git.git";
+    public static String REPO_LINK = "https://github.com/Uchiha8/gittask.git";
 }
